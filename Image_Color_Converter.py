96ad99efca98af7a4ef6acc81a8bb1d69151f45c
smeninger101 <75189724+smeninger101@users.noreply.github.com>
2020-11-28 21:54:09 -0500

    Update Image_Color_Converter.py

--- Image_Color_Converter.py
+++ Image_Color_Converter.py
@@ -3,9 +3,14 @@ import os
 from os import listdir,makedirs
 from os.path import isfile,join
 
-path = r'C:\Users\smeni\OneDrive\Desktop\Fall_2020_Classes\Capstone\asl-alphabet\asl_alphabet_train\asl_alphabet_train\A' # Source Folder
-dstpath = r'C:\Users\smeni\OneDrive\Desktop\Fall_2020_Classes\Capstone\asl-alphabet\asl_alphabet_train_resize\A' # Destination Folder
+#Path to the Original Images
+path = r'C:\Users\smeni\OneDrive\Desktop\Fall_2020_Classes\Capstone\asl-alphabet\asl_alphabet_train\asl_alphabet_train\A' 
 
+#The Destination Path to put the Images in a new folder
+dstpath = r'C:\Users\smeni\OneDrive\Desktop\Fall_2020_Classes\Capstone\asl-alphabet\asl_alphabet_train_resize\A' 
+
+#Make a directory
+#If directory already exists, say displayed message
 try:
     makedirs(dstpath)
 except:
@@ -16,11 +21,22 @@ files = [f for f in listdir(path) if isfile(join(path,f))]
 
 for image in files:
     try:
+        
+        #Join the path to the images
         img = cv2.imread(os.path.join(path,image))
+        
         #thresh = 128
         #img_binary = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)[1]
+        
+        #Resize Images
         image = image.resize((64, 64))
+        
+        #Join modified images to the Destination Path
         dstPath = join(dstpath,image)
+        
+        #Put the new images in the Destination Path
         cv2.imwrite(dstPath,image)
     except:
+        
+        #If the images won't convert, say displayed message
         print ("{} is not converted".format(image))
